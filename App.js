80c016957cf4dd71b5d380d79db36cd7ff897565
Zaynzakirhussain <rizwana.parveen411@gmail.com>
2021-02-12 20:16:55 +0530

    One For All

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import FbScreen from './screens/FbScreen';
+import InScreen from './screens/InScreen';
+
+export default class App extends React.Component {
+  render() {
+    return (
+
+      <AppContainer />
+
+    );
+  }
 }
+
+const TabNavigator = createBottomTabNavigator({
+  Fb: { screen: FbScreen },
+  In: { screen: InScreen },
+},
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: () => {
+        const routeName = navigation.state.routeName;
+        if (routeName === "Fb") {
+          return (
+            <Image
+              source={require("./assets/FB.jpg")}
+              style={{ width: 40, height: 40 }}
+            />
+          )
+
+        }
+        else if (routeName === "In") {
+          return (
+            <Image
+              source={require("./assets/IN.jpg")}
+              style={{ width: 40, height: 40 }}
+            />)
+
+        }
+      }
+    })
+  }
+);
+
+const AppContainer = createAppContainer(TabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ screens/FbScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default class FbScreen extends React.Component {
+    render() {
+      return (
+        <View>
+            <Text>ghrfvjj</Text>
+        </View>
+      );
+    }
+  }
